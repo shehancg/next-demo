06ce261650968806fe99d544334dfbde9a1c04ce
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-09-17 23:47:12 +0530

    fixed error in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Link from 'next/dist/client/link';
 
 export default function Home({ formattedDate }: { formattedDate: string }) {
   return (
@@ -8,9 +7,7 @@ export default function Home({ formattedDate }: { formattedDate: string }) {
       <h1>Static page</h1>
       <p>This page is static. It was built on {formattedDate}.</p>
       <p>
-      <Link href="./ssr">
-          <a>About Page</a>
-        </Link>
+        <a href="../pages/SSR">View a server-side rendered page.</a>
       </p>
     </>
   );
